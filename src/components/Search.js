7c81ccb48384b55d4898616fbb7db56ee9d0d77e
xiaolu sun <xiaolusun@xiaolus-MBP.fios-router.home>
2020-06-17 23:24:26 -0400

    import axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,15 @@
 import React from  "react";
-
+import axios from "axios";
 
 class Search extends React.Component{
     state = {
+
+    }
+    componentDidMount = () => {
+        axios.get("https://randomuser.me/api/?results=200&nat=us")
+         .then(response => {
+            console.log(response.data)
+         });
 
     }
     render(){
